45be8ca22ac78c0b89852ee53fc9a755195430a4
James <jamescoldwell73@gmail.com>
2022-03-16 11:38:51 +1030

    Update 3

---
+++ Bureaucrat.cpp
@@ -0,0 +1,11 @@
+#include "Computer.h"
+#include "Bureaucrat.h"
+
+
+Bureaucrat::Bureaucrat() {
+
+}
+
+char Bureaucrat::makeMove() {
+    return 'P';
+}
---
+++ Bureaucrat.h
@@ -0,0 +1,18 @@
+#ifndef H_Bur
+#define H_Bur
+
+#include "Computer.h"
+
+
+class Bureaucrat : public Computer {
+public:
+    Bureaucrat();
+    char makeMove();
+
+private:
+
+
+};
+
+
+#endif
